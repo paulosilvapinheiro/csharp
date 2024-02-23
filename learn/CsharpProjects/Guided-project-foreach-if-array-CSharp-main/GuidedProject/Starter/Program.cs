9ee21c6dbef59cc18994fa681b42fab02c375a29
paulo <paulo@paulo>
2024-02-23 09:04:47 -0300

    Realizado exercicio de nota dos alunos

--- learn/CsharpProjects/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
+++ learn/CsharpProjects/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -2,56 +2,103 @@
 
 // initialize variables - graded assignments 
 int currentAssignments = 5;
+string currentStudentLetterGrade = "";
+int[] notasSofia = new int[]{90,86,87,98,100,94,90};
+int[] notasAndrew = new int[]{92,89,81,96,90,89};
+int[] notasEmma = new int[]{90,85,87,98,68,89,89,89};
+int[] notasLogan = new int[]{90,95,87,88,96,96};
+int[] notasBecky = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+int[] notasChris = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+int[] notasEric = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
+int[] notasGregor = new int[] { 91, 91, 91, 91, 91, 91, 91 };    
 
-int sophia1 = 90;
-int sophia2 = 86;
-int sophia3 = 87;
-int sophia4 = 98;
-int sophia5 = 100;
+int[] notasAlunos = new int[10];
 
-int andrew1 = 92;
-int andrew2 = 89;
-int andrew3 = 81;
-int andrew4 = 96;
-int andrew5 = 90;
-
-int emma1 = 90;
-int emma2 = 85;
-int emma3 = 87;
-int emma4 = 98;
-int emma5 = 68;
-
-int logan1 = 90;
-int logan2 = 95;
-int logan3 = 87;
-int logan4 = 88;
-int logan5 = 96;
-
-int sophiaSum = 0;
-int andrewSum = 0;
-int emmaSum = 0;
-int loganSum = 0;
-
-decimal sophiaScore;
-decimal andrewScore;
-decimal emmaScore;
-decimal loganScore;
-
-sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
-emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
-loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
-
-sophiaScore = (decimal)sophiaSum / currentAssignments;
-andrewScore = (decimal)andrewSum / currentAssignments;
-emmaScore = (decimal)emmaSum / currentAssignments;
-loganScore = (decimal)loganSum / currentAssignments;
+// Student names
+string[] alunos = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
 
 Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+
+foreach(string aluno in alunos){
+
+    if(aluno == "Sophia"){
+        notasAlunos = notasSofia;
+    }
+    else if(aluno == "Andrew"){
+        notasAlunos = notasAndrew;
+    }
+    else if(aluno == "Emma"){
+        notasAlunos = notasEmma;
+    }
+    else if(aluno == "Logan"){
+        notasAlunos = notasLogan;
+    }
+    else if (aluno == "Becky")
+        notasAlunos = notasBecky;
+    else if (aluno == "Chris")
+        notasAlunos = notasChris;
+    else if (aluno == "Eric")
+        notasAlunos = notasEric;
+    else if (aluno == "Gregor")
+        notasAlunos = notasGregor;
+    // else
+    //     continue;
+
+    int sumNota = 0;
+    int countNota = 1;
+    foreach(int nota in notasAlunos){
+        
+        if (countNota < 6){
+            sumNota += nota;
+        }else{
+            sumNota += (int)(nota * 0.1);
+        }
+        countNota++;
+    }
+
+    decimal scoreFinal = (decimal)sumNota/currentAssignments;
+
+    if (scoreFinal >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (scoreFinal >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (scoreFinal >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (scoreFinal >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (scoreFinal >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (scoreFinal >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (scoreFinal >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (scoreFinal >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (scoreFinal >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (scoreFinal >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (scoreFinal >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (scoreFinal >= 60)
+        currentStudentLetterGrade = "D-";
+
+    else
+        currentStudentLetterGrade = "F";
+
+    Console.WriteLine($"{aluno}:\t\t{scoreFinal}\t{currentStudentLetterGrade}");
+}
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
